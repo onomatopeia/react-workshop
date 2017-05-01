ea007a6d24df32f0a28fa25392ec3120a0801c3b
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-05-02 00:54:37 +0200

    Cleaned up Home page

--- src/Home/index.js
+++ src/Home/index.js
@@ -5,63 +5,71 @@ export default () => (
   <div>
     <h1>React is undefined</h1>
 
-    <h2>What is it, how does it work, what’s the point? <em>(30 mins)</em></h2>
+    <p>
+      Welcome to this introduction to React! In the next few hours, we’ll see
+      what is React, why would we use it over some over technologies, and how to
+      get started with writing React components.
+    </p>
 
-    <ul>
-    <li>
-      <p>Why choose React, what does it help with?</p>
+    <h2>What is React?</h2>
 
-      <ul>
-        <li>Declarative</li>
-        <li>Library, not a framework</li>
-        <li>Rich third-party ecosystem</li>
-      </ul>
-    </li>
-    <li>
-      <p>Reverse-tree based architecture</p>
+    <p>
+      TBA (not framework).
+    </p>
 
-      <ul>
-        <li>Root component</li>
-        <li>Sub-components…</li>
-      </ul>
-    </li>
-    <li>
-      <p>Component based approach with props</p>
-      <ul>
-        <li>
-          <Link to='/HelloWorld'>Hello World (without props)</Link>
-        </li>
-        <li>
-          <Link to='/SimpleProps'>Hello World (with props)</Link>
-        </li>
-        <li>
-          <Link to='/CompleteProps'>Moar props</Link>
-        </li>
-      </ul>
-    </li>
-    <li>
-      <p>React component lifecycle</p>
-      <ul>
-        <li>
-          <Link to='/State'>Stateful component</Link>
-        </li>
-      </ul>
-    </li>
-    <li>
-      <p>Higher Order Components</p>
-      <ul>
-        <li>
-          <Link to='/HigherOrder'>Higher-order components</Link>
-        </li>
-      </ul>
-    </li>
-    <li><p>Component state - container</p>
+    <h2>Why React?</h2>
 
-    <ul>
-    <li>https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0</li>
-    <li>More complex example, text filter component</li>
-    </ul></li>
-    </ul>
+    <p>
+      TBA (declarative, rich ecosystem).
+    </p>
+
+    <h2>How does it work?</h2>
+
+    <p>
+      TBA (components, tree, diff…).
+    </p>
+
+    <h2>Components</h2>
+
+    <p>
+      Writing React components do not have to be scary. We will start with
+      a <Link to='/HelloWorld'>“Hello World”</Link> which renders the most basic
+      (and admittedly boring) component ever.
+    </p>
+
+    <p>
+      From there, we can get introduced to the <Link to='/SimpleProps'>concept of “props”</Link>.
+      We will go <Link to='/CompleteProps'>further with props</Link> until we are comfortable with them.
+    </p>
+
+    <h2>State & lifecycle</h2>
+
+    <p>
+      On top of props, React components can have internal state, making them
+      <Link to='/State'>“stateful components”</Link>. This is deeply connected
+      to their lifecycle.
+    </p>
+
+    <h2>Higher Order Components</h2>
+
+    <p>
+      A component based approach enables developer to create reusable blocks to
+      build consistent and efficient user interfaces. When it comes to
+      extracting logic, there is a concept
+      called <Link to='/HigherOrder'>higher-order components</Link>.
+    </p>
+
+    <h2>Components & containers</h2>
+
+    <p>
+      To avoid having components doing too many things, and to separate the
+      logic from the view, we use <Link to='/ManipulatingData'>“containers”</Link>.
+      Containers are regular components dealing with the logic bits and the data
+      transforming / sorting / filtering so the component underneath can focus
+      on rendering the data and displaying it in a nice way.
+    </p>
+
+    <h2>EVERYTHING BELOW IS TO CLEAN.</h2>
 
     <h2>Use this knowledge to build something <em>(60 mins)</em></h2>
 
