69f145ffe5df6ec6592e30fe183e5d1528f43b69
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-05-01 23:22:45 +0200

    Added more content to Props page

--- src/SimpleProps/index.js
+++ src/SimpleProps/index.js
@@ -4,9 +4,24 @@ import Code from '../__internals/Code'
 
 const Page = (props) => (
   <div>
-    <Title text='Component with simple props' />
+    <Title text='Props' />
 
-    <p>A React component can be seen as a function that accepts “props” (really just a fancy name for “data”) and that outpus some markup in the form of JSX (which is an optional abstraction on top of React internal API).</p>
+    <p>
+      As we have seen from the previous example, a React component can be seen
+      as a function that accepts “props” (really just a fancy name for “data”)
+      and that outpus some markup in the form of JSX.
+    </p>
+
+    <p>
+      All components, even those not using any props (like
+      our <code>HelloWorld</code> component from earlier) receives an object
+      (possibly empty) as props.
+    </p>
+
+    <p>
+      Let’s consider a <code>Title</code> component that would accept a text
+      prop. Here is how we could write it:
+    </p>
 
     <Code language='jsx'>{`import React from 'react'
 
@@ -14,9 +29,12 @@ const Title = (props) => <h1>{props.text}</h1>
 
 export default Title`}</Code>
 
-    <p>The title of this page is a component <code>Title</code> accepting a prop <code>text</code>. You can find the component definition in <code>Title.js</code>.</p>
+    <p>
+      This is how the title of this page has been rendered. You can find the
+      component definition in <code>Title.js</code>.
+    </p>
 
-    <Code language='jsx'>{`<Title text='Component with simple props' />`}</Code>
+    <Code language='jsx'>{`<Title text='Props' />`}</Code>
   </div>
 )
 
