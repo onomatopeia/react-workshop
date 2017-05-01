5607463ae47e47ab0eee3ef337e971885bcfc808
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-05-01 23:35:12 +0200

    Added a page of React Children

---
+++ src/Children/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Title from '../SimpleProps/Title'
+import Code from '../__internals/Code'
+
+const Page = (props) => (
+  <div>
+    <Title text='React Children' />
+
+    <p>
+      Generally speaking, you are free to name your props the way you want.
+      However there is one name you should avoid using for your own
+      props: <code>children</code>. This is “magic prop” that exists on all
+      React elements by default and that contains whatever is being rendered
+      between the opening tag and the closing tag (if any).
+    </p>
+
+    <p>
+      Remember how we made our <code>Title</code> component accept
+      a <code>text</code> prop to render it in a <code>h1</code>? It currently
+      looks like this:
+    </p>
+
+    <Code language='jsx'>{`<Title text='React Children' />`}</Code>
+
+    <p>
+      What if instead we could use our component like this:
+    </p>
+
+    <Code language='jsx'>{`<Title>React Children</Title>`}</Code>
+
+    <p>
+      Not only does it feel more natural but it also enables use to render
+      inner components if we need to (links, icons…). To do so, we can access
+      the content passed between tags with <code>props.children</code>.
+    </p>
+
+    <Code language='jsx'>{`const Title = (props) => <h1>{props.children}</h1>`}</Code>
+  </div>
+)
+
+export default Page
--- src/__internals/Router/index.js
+++ src/__internals/Router/index.js
@@ -7,6 +7,7 @@ import MoreProps from '../../CompleteProps'
 import State from '../../State'
 import HigherOrder from '../../HigherOrder'
 import Forms from '../../Forms'
+import Children from '../../Children'
 import './styles.css'
 
 class Navigation extends React.Component {
@@ -19,6 +20,7 @@ class Navigation extends React.Component {
             <li><Link to='/HelloWorld' className='Link'>Hello World</Link></li>
             <li><Link to='/SimpleProps' className='Link'>Props</Link></li>
             <li><Link to='/CompleteProps' className='Link'>More props</Link></li>
+            <li><Link to='/Children' className='Link'>React Children</Link></li>
             <li><Link to='/State' className='Link'>Stateful components</Link></li>
             <li><Link to='/HigherOrder' className='Link'>Higher-order functions</Link></li>
             <li><Link to='/Forms' className='Link'>Forms</Link></li>
@@ -29,6 +31,7 @@ class Navigation extends React.Component {
         <Route path='/HelloWorld' component={HelloWorld} />
         <Route path='/SimpleProps' component={Props} />
         <Route path='/CompleteProps' component={MoreProps} />
+        <Route path='/Children' component={Children} />
         <Route path='/State' component={State} />
         <Route path='/HigherOrder' component={HigherOrder} />
         <Route path='/Forms' component={Forms} />
