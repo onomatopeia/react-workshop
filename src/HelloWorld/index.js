6cbff8bf9d28c5901b8965fad2a675697effb741
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-05-01 23:18:05 +0200

    Added more content to HelloWorld page

--- src/HelloWorld/index.js
+++ src/HelloWorld/index.js
@@ -5,13 +5,31 @@ import Code from '../__internals/Code'
 const Page = (props) => (
   <div>
     <HelloWorld />
-    <p>This is the simplest React component example. To be fair, there is not so much things to see here… You can look at the code in <code>HelloWorld.js</code> and maybe play with it if you’d like!</p>
+
+    <p>
+      A React component —in its simplest form— is a function that outputs
+      markup. This is awfully simplistic but hopefully that describes simply
+      enough how React works as a “rendering system”.
+    </p>
 
     <Code language='jsx'>{`import React from 'react'
 
 const HelloWorld = () => <h1>Hello world</h1>
 
 export default HelloWorld`}</Code>
+
+    <p>
+      JSX is an abstraction on top of React internal API (
+      namely <code>React.createElement</code>) and while not mandatory at all,
+      makes authoring components much nicer. You can learn more <a href='https://facebook.github.io/react/docs/jsx-in-depth.html' target='_blank'>
+      about JSX in React’s docs</a>.
+    </p>
+
+    <p>
+      You can look at the code in <code>HelloWorld.js</code> and maybe play with
+      it if you’d like!
+    </p>
+
   </div>
 )
 
