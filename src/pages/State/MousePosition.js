f59c0ffa5bdb7eccdf6421ffdb4cb78163bc3dce
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-05-12 13:37:52 +0200

    Added hint to MousePosition component

--- src/pages/State/MousePosition.js
+++ src/pages/State/MousePosition.js
@@ -4,6 +4,8 @@ class MousePosition extends React.Component {
   render () {
     return (
       // Adjust this to log the current mouse position on screen.
+      // Hint: window.addEventListener('mousemove', …)
+      // Hint: event.clientX, event.clientY
       <p>Mouse is currently at position X:Y.</p>
     )
   }
