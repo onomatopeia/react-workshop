d97b049425a10672b77da855a9287a07de0e1f8a
Hugo Giraudel <hugo.giraudel@gmail.com>
2018-03-10 16:23:48 +0100

    Update link to React

--- src/pages/1-Home/index.js
+++ src/pages/1-Home/index.js
@@ -5,21 +5,25 @@ import SectionTitle from '../../components/SectionTitle'
 
 export default () => (
   <div>
-    <PageTitle text='React is undefined' />
+    <PageTitle text="React is undefined" />
 
     <p>
-      Welcome to this introduction to <a href='https://facebook.github.io/react/' target='_blank' rel='noopener noreferrer' >
-      React</a>! In the next few hours, we’ll see what is React, why would we
-      use it over some over technologies, and how to get started with writing
-      React components.
+      Welcome to this introduction to{' '}
+      <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+        React
+      </a>! In the next few hours, we’ll see what is React, why would we use it
+      over some over technologies, and how to get started with writing React
+      components.
     </p>
 
     <p>
-      <strong>✋ If you have any question at any moment, please be sure to ask! We are
-      here to help.</strong>
+      <strong>
+        ✋ If you have any question at any moment, please be sure to ask! We are
+        here to help.
+      </strong>
     </p>
 
-    <SectionTitle text='What is React?' />
+    <SectionTitle text="What is React?" />
 
     <p>
       React is a JavaScript library for building user interfaces. It differs
@@ -29,7 +33,7 @@ export default () => (
       interface.
     </p>
 
-    <SectionTitle text='Why React?' />
+    <SectionTitle text="Why React?" />
 
     <p>
       There are many reasons why you would pick React over some other library or
@@ -42,11 +46,11 @@ export default () => (
     <p>
       In React, almost everything is a component. This heavily componentized
       approach makes it the perfect candidate to build complex layouts with a
-      lot of tiny reusable pieces. 
+      lot of tiny reusable pieces.
     </p>
 
     <p>
-      One of the benefits of embracing React is also its very rich ecosystem of 
+      One of the benefits of embracing React is also its very rich ecosystem of
       components and plugins. Many companies have implemented React in their
       tech stack and open-source their components for broader use.
     </p>
@@ -59,89 +63,97 @@ export default () => (
       engines, in Sketch, in augmented reality…
     </p>
 
-    <SectionTitle text='How does it work?' />
+    <SectionTitle text="How does it work?" />
 
     <p>
-      React abstracts the <abbr title='Document Object Model'>DOM</abbr> away.
+      React abstracts the <abbr title="Document Object Model">DOM</abbr> away.
       It operates over a “virtual DOM”. Whenever one of your components gets
       rendered, it doesn’t get written into the HTML document just yet. It gets
       rendered inside a virtual representation of the DOM. Eventually, React
       will convert that representation into actual HTML. While it might sound
-      unnecessarily complex, this is how React can offer such a simple and 
+      unnecessarily complex, this is how React can offer such a simple and
       straight-forward declarative API.
     </p>
 
     <p>
       Now when it comes to know *what* to render, React does a “reconciliation”.
       This is the process in which React compares two virtual DOM trees to
-      figure out what has changed and what needs to be refreshed. You can
-      read <a href='https://facebook.github.io/react/docs/reconciliation.html' target='_blank' rel='noopener noreferrer'>about the reconciliation process
-      </a> more in details in the official documentation.
+      figure out what has changed and what needs to be refreshed. You can read{' '}
+      <a
+        href="https://facebook.github.io/react/docs/reconciliation.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        about the reconciliation process
+      </a>{' '}
+      more in details in the official documentation.
     </p>
 
-    <SectionTitle text='Components' />
+    <SectionTitle text="Components" />
 
     <p>
-      Writing React components do not have to be scary. We will start with
-      a <Link to='/HelloWorld'>“Hello World”</Link> which renders the most basic
+      Writing React components do not have to be scary. We will start with a{' '}
+      <Link to="/HelloWorld">“Hello World”</Link> which renders the most basic
       (and admittedly boring) component ever.
     </p>
 
     <p>
-      From there, we can get introduced to the <Link to='/SimpleProps'>concept
-      of “props”</Link>. We will go <Link to='/CompleteProps'>further with
-      props</Link> until we are comfortable with them. Components configured
-      with props being no different than functions configured with parameters,
-      we should be able to quickly move to the interesting bits!
+      From there, we can get introduced to the{' '}
+      <Link to="/SimpleProps">concept of “props”</Link>. We will go{' '}
+      <Link to="/CompleteProps">further with props</Link> until we are
+      comfortable with them. Components configured with props being no different
+      than functions configured with parameters, we should be able to quickly
+      move to the interesting bits!
     </p>
 
-    <SectionTitle text='Lifecycle' />
+    <SectionTitle text="Lifecycle" />
 
     <p>
-      Every React component has some sort of <Link to='/Lifecycle'>lifecycle</Link>.
-      It is possible to hook things onto these lifecycle events in order to
-      react to certain moments in time (when the component gets mounted,
-      unmounted, updated with new props…).
+      Every React component has some sort of{' '}
+      <Link to="/Lifecycle">lifecycle</Link>. It is possible to hook things onto
+      these lifecycle events in order to react to certain moments in time (when
+      the component gets mounted, unmounted, updated with new props…).
     </p>
 
-    <SectionTitle text='State' />
+    <SectionTitle text="State" />
 
     <p>
-      On top of props, React components can have internal state, making
-      them <Link to='/State'>“stateful components”</Link>. This is deeply
-      connected to their lifecycle. This is also what makes it possible to
-      write dynamic applications maintaining (local and/or global) states. In
-      other words, this is where the fun begins!
+      On top of props, React components can have internal state, making them{' '}
+      <Link to="/State">“stateful components”</Link>. This is deeply connected
+      to their lifecycle. This is also what makes it possible to write dynamic
+      applications maintaining (local and/or global) states. In other words,
+      this is where the fun begins!
     </p>
 
-    <SectionTitle text='Higher Order Components' />
+    <SectionTitle text="Higher Order Components" />
 
     <p>
       A component based approach enables developer to create reusable blocks to
       build consistent and efficient user interfaces. When it comes to
-      extracting logic, there is a concept
-      called <Link to='/HigherOrder'>higher-order components</Link>. We’ll see
-      how to write a higher-order function and how to use it. 
+      extracting logic, there is a concept called{' '}
+      <Link to="/HigherOrder">higher-order components</Link>. We’ll see how to
+      write a higher-order function and how to use it.
     </p>
 
-    <SectionTitle text='Components & containers' />
+    <SectionTitle text="Components & containers" />
 
     <p>
       To avoid having components doing too many things, and to separate the
-      logic from the view, we use <Link to='/ManipulatingData'>“containers”</Link>.
-      Containers are regular components dealing with the logic bits and the data
-      transforming / sorting / filtering so the component underneath can focus
-      on rendering the data and displaying it in a nice way.
+      logic from the view, we use{' '}
+      <Link to="/ManipulatingData">“containers”</Link>. Containers are regular
+      components dealing with the logic bits and the data transforming / sorting
+      / filtering so the component underneath can focus on rendering the data
+      and displaying it in a nice way.
     </p>
 
-    <SectionTitle text='Styling' />
+    <SectionTitle text="Styling" />
 
     <p>
       Styling in React apps can be done in many ways, from basic CSS in a
-      separate stylesheet to LESS/Sass and other preprocessors, up until CSS-in-JS
-      solutions! In this section we’ll experiment with of these JavaScript-powered
-      styling implementations and practice with authoring dynamic styles based
-      on components props.
+      separate stylesheet to LESS/Sass and other preprocessors, up until
+      CSS-in-JS solutions! In this section we’ll experiment with of these
+      JavaScript-powered styling implementations and practice with authoring
+      dynamic styles based on components props.
     </p>
   </div>
 )
