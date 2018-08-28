873ce6ad083a5859b989347ec9d1581e34be622a
Hugo Giraudel <hugo.giraudel@gmail.com>
2018-08-28 14:28:04 +0200

    Add Jevgenija to the list of users

--- src/pages/4-CompleteProps/UserIndex/index.js
+++ src/pages/4-CompleteProps/UserIndex/index.js
@@ -2,8 +2,13 @@ import React from 'react'
 import UserList from '../UserList'
 
 const users = [
-  { name: 'Michael', preferredName: 'Mike', email: 'mike@mikesmart.co.uk' },
-  { name: 'Hugo', email: 'hugo.giraudel@gmail.com' }
+  {
+    name: 'Jevgenija',
+    preferredName: 'Zhenya',
+    email: 'jevgenija.zigisova@n26.com'
+  },
+  { name: 'Michael', preferredName: 'Mike', email: 'mike.smart@n26.com' },
+  { name: 'Hugo', email: 'hugo.giraudel@n26.com' }
 ]
 
 const UserIndex = () => (
